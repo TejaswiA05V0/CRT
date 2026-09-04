714500a6300eb59ad25208000e91dda25a32d211
Gandhamoju Tejaswi <tejaswidts6@gmail.com>
2026-09-04 22:01:09 +0530

    Add grade evaluation based on student score

---
+++ Student Grade.py
@@ -0,0 +1,11 @@
+n=int(input())
+if n>=90:
+    print("Excellent")
+elif n>=75 and n<=89:
+    print("very good")
+elif n>=60 and n<=74:
+    print("Good")
+elif n>=40 and n<=59:
+    print("Average")
+else:
+    print("Fail")
